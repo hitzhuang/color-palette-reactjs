5eae166a62bf9036c46a112858c61fe5a010bbdd
Hitz Huang <hitz@pjx-studios.com>
2020-08-10 15:05:15 +0800

    Updates

---
+++ precache-manifest.a3c10e2b75b7a8863058bf25b977806c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54757cf5237cdb10dffe71f37690f1b",
+    "url": "/color-palette-reactjs/index.html"
+  },
+  {
+    "revision": "5618280f6731db3be6af",
+    "url": "/color-palette-reactjs/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "72a66d644c090fdef98b",
+    "url": "/color-palette-reactjs/static/css/main.ad45c265.chunk.css"
+  },
+  {
+    "revision": "5618280f6731db3be6af",
+    "url": "/color-palette-reactjs/static/js/2.e8d559da.chunk.js"
+  },
+  {
+    "revision": "5dbf9415e481a23d65662fd7dec5f7a6",
+    "url": "/color-palette-reactjs/static/js/2.e8d559da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a66d644c090fdef98b",
+    "url": "/color-palette-reactjs/static/js/main.ed274267.chunk.js"
+  },
+  {
+    "revision": "9da12ffcd3bb36627f4a",
+    "url": "/color-palette-reactjs/static/js/runtime-main.bd9c3e1e.js"
+  }
+]);
