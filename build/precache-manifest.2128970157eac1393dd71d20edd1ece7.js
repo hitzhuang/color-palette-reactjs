ad0eb3d53514225887e186678ffa3a3b67adbbc2
Hitz Huang <hitz@pjx-studios.com>
2022-04-28 11:05:02 +0800

    update axios package

---
+++ build/precache-manifest.2128970157eac1393dd71d20edd1ece7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a517d26d2a183c03ff20e7a1d0be94f",
+    "url": "/color-palette-reactjs/index.html"
+  },
+  {
+    "revision": "340db108acc37c3ee9d8",
+    "url": "/color-palette-reactjs/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "4524daea9f2864c825a0",
+    "url": "/color-palette-reactjs/static/css/main.ad45c265.chunk.css"
+  },
+  {
+    "revision": "340db108acc37c3ee9d8",
+    "url": "/color-palette-reactjs/static/js/2.ee44f6ea.chunk.js"
+  },
+  {
+    "revision": "5dbf9415e481a23d65662fd7dec5f7a6",
+    "url": "/color-palette-reactjs/static/js/2.ee44f6ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4524daea9f2864c825a0",
+    "url": "/color-palette-reactjs/static/js/main.8b747adf.chunk.js"
+  },
+  {
+    "revision": "9da12ffcd3bb36627f4a",
+    "url": "/color-palette-reactjs/static/js/runtime-main.bd9c3e1e.js"
+  }
+]);
